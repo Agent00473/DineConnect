ad8f5c7c66b1bfeb0186f59d0e7532253756d105
Agent00473 <135093539+Agent00473@users.noreply.github.com>
2024-11-27 01:01:43 +0100

    Service and Models

---
+++ DineConnect.PaymentManagementService.Application/Common/Interfaces/IPromotionService.cs
@@ -0,0 +1,12 @@
+﻿
+using DineConnect.PaymentManagementService.Application.Common.Responses;
+
+namespace DineConnect.PaymentManagementService.Application.Common.Interfaces
+{
+
+    public interface IPromotionService 
+    {
+        FlashSaleResponseModel GetFlashSaleDetails<TRequest>(TRequest request) ;
+        DiscountResponseModel GetDiscountDetails<TRequest>(TRequest request);
+    }
+}
---
+++ DineConnect.PaymentManagementService.Application/Common/Responses/DiscountResponseModel.cs
@@ -0,0 +1,25 @@
+﻿
+namespace DineConnect.PaymentManagementService.Application.Common.Responses
+{
+    public record DiscountResponseModel
+    {
+        public decimal DiscountAmount { get; init; }
+        public decimal FinalPrice { get; init; }
+        public string Message { get; init; }
+
+        // Private constructor
+        private DiscountResponseModel(decimal discountAmount, decimal finalPrice, string message)
+        {
+            DiscountAmount = discountAmount;
+            FinalPrice = finalPrice;
+            Message = message;
+        }
+
+        // Static Create method to instantiate the record
+        public static DiscountResponseModel Create(decimal discountAmount, decimal finalPrice, string message)
+        {
+            return new DiscountResponseModel(discountAmount, finalPrice, message);
+        }
+    }
+
+}
