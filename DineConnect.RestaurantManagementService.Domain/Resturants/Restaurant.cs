5f853fabe8a00ba9547a158ed75a9be625ca2dce
Agent00473 <135093539+Agent00473@users.noreply.github.com>
2025-01-21 23:57:28 +0100

    Aggregate Root

---
+++ DineConnect.RestaurantManagementService.Domain/Resturants/Restaurant.cs
@@ -0,0 +1,50 @@
+﻿using DineConnect.RestaurantManagementService.Domain.Catalogues.ValueObjects;
+using DineConnect.RestaurantManagementService.Domain.Orders.ValueObjects;
+using Infrastructure.Domain.Entities;
+
+namespace DineConnect.RestaurantManagementService.Domain.Resturants
+{
+    public class Restaurant : BaseAggregateRoot<RestaurantId, Guid>
+    {
+        #region Constants and Static Fields
+        // Add constants and static fields here
+        #endregion
+
+        #region Private & Protected Fields
+        #endregion
+
+        #region Private & Protected Methods
+        // Add protected methods here
+        #endregion
+
+
+        #region Constructors
+        public Restaurant(RestaurantId id, CustomerId customerId, CatalogueId catalogueId): base(id)
+        {
+            CustomerId = customerId;
+            CatalogueId = catalogueId;
+        }
+        #endregion
+
+        #region Public Properties
+        public string Name { get; private set; }
+        public CustomerId CustomerId { get; private set; }
+        public CatalogueId CatalogueId { get; private set; }
+
+        #endregion
+
+        #region Public Methods
+        #endregion
+
+        #region Interface Implementations
+        #endregion
+
+        #region Factory Methods
+        // Factory Method
+        public static Restaurant Create(CustomerId customerId, CatalogueId catalogueId)
+        {
+            return new Restaurant(RestaurantId.Create(), customerId, catalogueId);
+        }
+        #endregion
+    }
+}
---
+++ DineConnect.RestaurantManagementService.Domain/Resturants/RestaurantId.cs
@@ -0,0 +1,34 @@
+﻿using Infrastructure.Domain.Entities;
+
+namespace DineConnect.RestaurantManagementService.Domain.Resturants
+{
+    public class RestaurantId : BaseAggregateRootId<Guid>
+    {
+        private RestaurantId(Guid value)
+        {
+            IdValue = value;
+        }
+
+        private RestaurantId()
+        {
+        }
+
+        public override Guid IdValue { get; protected set; }
+
+        public override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return IdValue;
+        }
+
+        public static RestaurantId Create()
+        {
+            return new(Guid.NewGuid());
+        }
+
+        public static RestaurantId Create(Guid value)
+        {
+            return new(value);
+        }
+    }
+
+}
